180d8bbc10714364ae22cc4c2dfc6b79e35b6544
Luca Tumedei <luca@theaveragedev.com>
2015-03-12 20:13:28 +0100

    modified script and style handles

--- src/Multi_Image_Control_Register.php
+++ src/Multi_Image_Control_Register.php
@@ -11,8 +11,12 @@
 			$postfix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 			$js_src = plugins_url( '/js/multi-image' . $postfix . '.js', dirname( __FILE__ ) );
 			$css_src = plugins_url( '/css/multi-image' . $postfix . '.css', dirname( __FILE__ ) );
-			wp_register_script( 'tad-multi-image-control', $js_src, array( 'jquery', 'backbone', 'underscore' ) );
-			wp_register_style( 'tad-multi-image-control', $css_src );
+			wp_register_script( 'multi-image-control-js', $js_src, array(
+				'jquery',
+				'backbone',
+				'underscore'
+			), null, true );
+			wp_register_style( 'multi-image-control-css', $css_src );
 		}
 
 		public function register_control_type( WP_Customize_Manager $wp_customize ) {
