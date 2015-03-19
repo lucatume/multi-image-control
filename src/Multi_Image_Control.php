870f14a1368a96fb4fccf77b326f890632bd6d01
Luca Tumedei <luca@theaveragedev.com>
2015-03-19 18:50:18 +0100

    removed deprecated methods

--- src/Multi_Image_Control.php
+++ src/Multi_Image_Control.php
@@ -24,24 +24,10 @@ class tad_Multi_Image_Control extends WP_Customize_Control
     public function to_json()
     {
         parent::to_json();
-        $this->json['value'] = $this->value();
-        $this->json['srcs'] = $this->get_srcs();
-        $this->json['link'] = $this->get_link();
         $this->json['l10n']['upload_button_label'] = __('Upload', 'mic');
         $this->json['l10n']['remove_all_button_label'] = __('Remove all', 'mic');
     }
 
-	protected function get_srcs() {
-		$imageSrcs = explode( ',', $this->value() );
-		if ( ! is_array( $imageSrcs ) ) {
-			$imageSrcs = array();
-
-			return $imageSrcs;
-		}
-
-		return $imageSrcs;
-	}
-    
     public function render_content()
     {
         // no op
